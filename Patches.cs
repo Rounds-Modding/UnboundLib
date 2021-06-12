0b2bbe6bbf78fd1d513859b8df7f2db2a8ad63ed
willis81808 <willis81808@gmail.com>
2021-06-12 16:36:30 -0600

    fixed issue with mod cards not showing up in card bar. improved scrolling in card toggle window

--- Patches.cs
+++ Patches.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using HarmonyLib;
+using UnityEngine;
 using UnboundLib.Cards;
 
 namespace UnboundLib
@@ -34,6 +35,15 @@ namespace UnboundLib
         static void Prefix(int teamId, CardInfo card)
         {
             CardData.AddCard(teamId, card.cardName);
+        }
+    }
+
+    [HarmonyPatch(typeof(CardBar), "OnHover")]
+    class CardBar_Patch
+    {
+        static void Postfix(CardBar __instance, CardInfo card, Vector3 hoverPos, GameObject ___currentCard)
+        {
+            ___currentCard.SetActive(true);
         }
     }
 }
