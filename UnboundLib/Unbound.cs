6eac6995f6c118497732e7f1fb7b5e3b1bd23f0a
Olavi Mustanoja <tilastokeskus@gmail.com>
2021-07-07 09:16:10 +0300

    Bump version to 2.2.0

--- UnboundLib/Unbound.cs
+++ UnboundLib/Unbound.cs
@@ -20,7 +20,7 @@ namespace UnboundLib
     {
         private const string ModId = "com.willis.rounds.unbound";
         private const string ModName = "Rounds Unbound";
-        public const string Version = "2.1.5";
+        public const string Version = "2.2.0";
 
         public static Unbound Instance { get; private set; }
 
