39fd44646080277a5cf0585d9dcbd1efcffacf70
pdcook <qphysicscook@gmail.com>
2022-03-28 21:56:32 -0400

    add CustomCard::Callback

--- UnboundLib/Cards/CustomCard.cs
+++ UnboundLib/Cards/CustomCard.cs
@@ -34,22 +34,7 @@ namespace UnboundLib.Cards
         {
             if (!isPrefab)
             {
-                // add mod name text
-                // create blank object for text, and attach it to the canvas
-                GameObject modNameObj = new GameObject("ModNameText");
-                // find bottom left edge object
-                RectTransform[] allChildrenRecursive = this.gameObject.GetComponentsInChildren<RectTransform>();
-                GameObject BottomLeftCorner = allChildrenRecursive.Where(obj => obj.gameObject.name == "EdgePart (2)").FirstOrDefault().gameObject;
-                modNameObj.gameObject.transform.SetParent(BottomLeftCorner.transform);
-                TextMeshProUGUI modText = modNameObj.gameObject.AddComponent<TextMeshProUGUI>();
-                modText.text = this.GetModName().Sanitize();
-                modNameObj.transform.Rotate(new Vector3(0f, 0f, 1f), 45f);
-                modNameObj.transform.Rotate(new Vector3(0f, 1f, 0f), 180f);
-                modNameObj.transform.localScale = new Vector3(1f, 1f, 1f);
-                modNameObj.AddComponent<SetLocalPos>();
-                modText.alignment = TextAlignmentOptions.Bottom;
-                modText.alpha = 0.1f;
-                modText.fontSize = 54;
+                Destroy(transform.GetChild(1).gameObject);
 
                 this.Callback();
             }
@@ -76,16 +61,10 @@ namespace UnboundLib.Cards
         {
             OnRemoveCard();
         }
-        /// <summary>
-        /// Returns if the card should be enabled when built. Cards that are not enabled do not appear in the Toggle Cards menu, nor can be spawned in game by any regular means
-        /// </summary>
         public virtual bool GetEnabled()
         {
             return true;
         }
-        /// <summary>
-        /// Returns the name of the mod this card is from. Should be unique.
-        /// </summary>
         public virtual string GetModName()
         {
             return "Modded";
@@ -95,7 +74,7 @@ namespace UnboundLib.Cards
         /// </summary>
         public virtual void Callback()
         {
-            
+
         }
 
         public static void BuildCard<T>() where T : CustomCard
@@ -108,15 +87,16 @@ namespace UnboundLib.Cards
             {
                 // Instantiate card and mark to avoid destruction on scene change
                 var newCard = Instantiate(Unbound.templateCard.gameObject, Vector3.up * 100, Quaternion.identity);
-                DestroyImmediate(newCard.transform.GetChild(0).gameObject);
                 newCard.transform.SetParent(null, true);
                 var newCardInfo = newCard.GetComponent<CardInfo>();
-                newCardInfo.cardBase = Unbound.templateCard.cardBase;
                 DontDestroyOnLoad(newCard);
 
                 // Add custom ability handler
                 var customCard = newCard.AddComponent<T>();
                 customCard.isPrefab = true;
+
+                // Clear default card info
+                DestroyChildren(newCardInfo.cardBase.GetComponent<CardInfoDisplayer>().grid);
 
                 // Apply card data
                 newCardInfo.cardStats = customCard.GetStats() ?? new CardInfoStat[0];
@@ -128,11 +108,33 @@ namespace UnboundLib.Cards
                 newCardInfo.allowMultiple = true;
                 newCardInfo.cardArt = customCard.GetCardArt();
 
+                // add mod name text
+                // create blank object for text, and attach it to the canvas
+                GameObject modNameObj = new GameObject("ModNameText");
+                // find bottom left edge object
+                RectTransform[] allChildrenRecursive = newCard.gameObject.GetComponentsInChildren<RectTransform>();
+                GameObject BottomLeftCorner = allChildrenRecursive.Where(obj => obj.gameObject.name == "EdgePart (2)").FirstOrDefault().gameObject;
+                modNameObj.gameObject.transform.SetParent(BottomLeftCorner.transform);
+                TextMeshProUGUI modText = modNameObj.gameObject.AddComponent<TextMeshProUGUI>();
+                modText.text = customCard.GetModName().Sanitize();
+                modNameObj.transform.Rotate(new Vector3(0f, 0f, 1f), 45f);
+                modNameObj.transform.Rotate(new Vector3(0f, 1f, 0f), 180f);
+                modNameObj.transform.localScale = new Vector3(1f, 1f, 1f);
+                modNameObj.AddComponent<SetLocalPos>();
+                modText.alignment = TextAlignmentOptions.Bottom;
+                modText.alpha = 0.1f;
+                modText.fontSize = 54;
+
+
+                // Fix sort order issue
+                newCardInfo.cardBase.transform.position -= Camera.main.transform.forward * 0.5f;
 
                 // Reset stats
                 newCard.GetComponent<CharacterStatModifiers>().health = 1;
 
+
                 // Finish initializing
+                newCardInfo.SendMessage("Awake");
                 PhotonNetwork.PrefabPool.RegisterPrefab(newCard.gameObject.name, newCard);
 
                 // If the card is enabled
@@ -147,12 +149,24 @@ namespace UnboundLib.Cards
                         new Card(customCard.GetModName().Sanitize(), Unbound.config.Bind("Cards: " + customCard.GetModName().Sanitize(), newCardInfo.cardName, true), newCardInfo));
                 }
 
-                
+
 
                 // Post-creation clean up
                 newCardInfo.ExecuteAfterFrames(5, () =>
                 {
+                    // Destroy extra card face
+                    Destroy(newCard.transform.GetChild(0).gameObject);
+
+                    // Destroy extra art object
+                    var artContainer = newCard.transform.Find("CardBase(Clone)(Clone)/Canvas/Front/Background/Art");
+                    if (artContainer != null && artContainer.childCount > 1)
+                        Destroy(artContainer.GetChild(0).gameObject);
+
+                    // Disable "prefab"
+                    newCard.SetActive(false);
+
                     callback?.Invoke(newCardInfo);
+
                 });
             });
         }
@@ -275,7 +289,7 @@ namespace UnboundLib.Cards
                 if (gameObject.transform.localPosition != localpos)
                 {
                     gameObject.transform.localPosition = localpos;
-                    Destroy(this,1f);
+                    Destroy(this, 1f);
                 }
             }
         }
