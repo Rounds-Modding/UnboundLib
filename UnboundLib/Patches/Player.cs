35dcd117cf64743f12302917991eefb14595faed
Tess <tess@tessyac.com>
2022-12-28 18:21:22 -0500

    Rest player patch

--- UnboundLib/Patches/Player.cs
+++ UnboundLib/Patches/Player.cs
@@ -1,10 +1,13 @@
 ﻿using HarmonyLib;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
+using UnboundLib.Cards;
 using UnboundLib.Extensions;
 using UnboundLib.GameModes;
+using UnboundLib.Utils;
 
 namespace UnboundLib.Patches
 {
@@ -102,6 +105,38 @@ namespace UnboundLib.Patches
                     yield return ins;
                 }
             }
+        }
+    }
+    [HarmonyPatch(typeof(Player), "FullReset")]
+    class Player_Patch_FullReset
+    {
+        static void Postfix(Player __instance)
+        {
+            foreach (CardInfo currentCard in __instance.data.currentCards)
+            {
+                if(currentCard.GetComponent<CustomCard>() is CustomCard customCard)
+                {
+                    try
+                    {
+                        Gun gun = __instance.GetComponent<Holding>().holdable.GetComponent<Gun>();
+                        CharacterData characterData = __instance.GetComponent<CharacterData>();
+                        HealthHandler healthHandler = __instance.GetComponent<HealthHandler>();
+                        Gravity gravity = __instance.GetComponent<Gravity>();
+                        Block block = __instance.GetComponent<Block>();
+                        GunAmmo gunAmmo = gun.GetComponentInChildren<GunAmmo>();
+                        CharacterStatModifiers characterStatModifiers = __instance.GetComponent<CharacterStatModifiers>();
+                        customCard.OnRemoveCard(__instance, gun, gunAmmo, characterData, healthHandler, gravity, block, characterStatModifiers);
+                    }
+                    catch (NotImplementedException)
+                    { }
+                    catch (Exception exception)
+                    {
+                        UnityEngine.Debug.LogError($"{exception.GetType()}\nThrown by: {customCard.GetModName()} - {currentCard.cardName} - OnRemoveCard()");
+                        UnityEngine.Debug.LogException(exception);
+                    }
+                }
+            }
+            __instance.data.currentCards.Clear();
         }
     }
 }
