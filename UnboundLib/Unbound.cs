16d6f596bc213b04e92363a297992f3d1cdfd678
Olavi Mustanoja <tilastokeskus@gmail.com>
2023-11-30 17:55:03 +0200

    v3.2.13

--- UnboundLib/Unbound.cs
+++ UnboundLib/Unbound.cs
@@ -25,7 +25,7 @@ namespace UnboundLib
     {
         private const string ModId = "com.willis.rounds.unbound";
         private const string ModName = "Rounds Unbound";
-        public const string Version = "3.2.12";
+        public const string Version = "3.2.13";
 
         public static Unbound Instance { get; private set; }
         public static readonly ConfigFile config = new ConfigFile(Path.Combine(Paths.ConfigPath, "UnboundLib.cfg"), true);
